9308b5b53b7a5b856e20fdde8ab588970943ade2
Enias Oliveira <eniasoliveira27@gmail.com>
2021-02-24 10:44:42 -0300

    Initial commit

---
+++ tests/test_serie_a.py
@@ -0,0 +1,6 @@
+from serie_a import create_serie_a_dict
+
+
+def test_create_serie_a_dict():
+
+    create_serie_a_dict()
---
+++ times/serie_a.py
@@ -0,0 +1,26 @@
+def create_serie_a_dict():
+
+    serie_a = [
+        "Athletico-PR",
+        "Atlético Goianiense",
+        "Atlético Mineiro",
+        "Bahia",
+        "Botafogo",
+        "Bragantino",
+        "Ceará",
+        "Corinthians",
+        "Coritiba",
+        "Flamengo",
+        "Fluminense",
+        "Fortaleza",
+        "Goiás",
+        "Grêmio",
+        "Internacional",
+        "Palmeiras",
+        "Santos",
+        "Sport Recife",
+        "São Paulo",
+        "Vasco da Gama",
+    ]
+
+    ...
